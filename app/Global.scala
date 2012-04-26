d6aa4903f61da6c2223faecee257745db77a6196
jtournay <jtournay@linkedin.com>
2012-04-25 18:17:44 -0700

    Major refactoring

--- app/Global.scala
+++ app/Global.scala
@@ -10,72 +10,103 @@ import java.security.SecureRandom
 import org.apache.commons.codec.binary._
 
 object CSRF {
+	type Token = String
+	
 	val TOKEN_NAME = "csrfToken"
 	val encoder = new Hex
 	val random = new SecureRandom
 
-	val INVALID_TOKEN  = Action(BadRequest("Invalid CSRF Token"))
+	val INVALID_TOKEN: PlainResult  = BadRequest("Invalid CSRF Token")
 	var UNSAFE_METHOD = "PUT|POST|DELETE".r
 
-	def generate = {
+	def generate: Token = {
 		val bytes = new Array[Byte](64)
 		random.nextBytes(bytes)
 		new String(encoder.encode(bytes), "UTF8")
 	}
 
 	def checkTokens(paramToken: String, sessionToken: String) = paramToken == sessionToken
-	
-	def wrap(request: RequestHeader, parent: Option[Handler]): Option[Handler] = {
-		def delegate: Option[Handler] = parent.map { h =>
-			h match {
-				case a: Action[_] => ResponseWrapper(a)
-				case _ => h
-			}
-		}
 
+	// -
+	def checkRequest(request: RequestHeader): Either[PlainResult, RequestHeader] = {
 		request.method match {
 			case UNSAFE_METHOD() => {
-				delegate.flatMap{ d =>
-					(for{ maybeTokens <- request.queryString.get(TOKEN_NAME);
-						token <- maybeTokens.headOption;
-						sessionToken <- request.session.get(TOKEN_NAME)
-					} yield if(checkTokens(token, sessionToken)) d else INVALID_TOKEN) orElse Some(INVALID_TOKEN)
-				}
+				(for{ maybeTokens <- request.queryString.get(TOKEN_NAME);
+					token <- maybeTokens.headOption;
+					sessionToken <- request.session.get(TOKEN_NAME)
+				} yield if(checkTokens(token, sessionToken)) Right(request) else Left(INVALID_TOKEN)) getOrElse Left(INVALID_TOKEN)
 			}
-			case _ => delegate
+			case _ => Right(request)
+		}
+	}
+
+	/**
+	* Add the session token to the Response if it's not already in the request
+	*/
+	def addSessionToken(req: RequestHeader, res: Result, token: Token): Result = res match {
+		case r: PlainResult => addSessionToken(req, r, token)
+		case r => r
+	}
+	def addSessionToken(req: RequestHeader, r: PlainResult, token: Token): PlainResult = {
+		if(req.session.get(TOKEN_NAME).isDefined){
+			r
+		}
+		else {
+			val session = Cookies(r.header.headers.get("Set-Cookie"))
+				.get(Session.COOKIE_NAME).map(_.value).map(Session.decode)
+				.getOrElse(Map.empty)
+			val newSession = if(session.contains(TOKEN_NAME)) session else (session + (TOKEN_NAME -> token))
+			r.withSession(Session.deserialize(newSession))
+		}
+	}
+
+	/**
+	* Add token to the request if necessary
+	*/
+	def addToken[A](request: Request[A], token: Token): Request[A] = request.session.get(TOKEN_NAME)
+		.map(_ => request)
+		.getOrElse(new WrappedRequest(request) {
+			override lazy val session = request.session + (TOKEN_NAME -> token) // TODO
+		})
+	def addToken[A](request: RequestHeader, token: Token): RequestHeader = addToken(EmptyRequest(request), token)
+
+	private[csrf] case class EmptyRequest(r: RequestHeader) extends Request[Unit]{
+		override def uri = r.uri
+		override def path = r.path
+		override def method = r.method
+		override def queryString = r.queryString
+		override def headers = r.headers
+		override def remoteAddress = r.remoteAddress
+		override def body = ()
+	}
+	
+	/**
+	* Add CSRF protection to a existing Handler
+	*/
+	def wrap(request: RequestHeader, parent: Option[Handler]): Option[Handler] = {
+		parent.map{
+			case a: Action[_] => ResponseWrapper(a)
+			case h => h
 		}
 	}
 }
 
 case class ResponseWrapper[A](action: Action[A]) extends Action[A] {
 	import CSRF._
-
 	lazy val token = CSRF.generate
 
-	def addSessionToken(req: RequestHeader, res: Result) = res match {
-		case r: PlainResult => {
-			if(req.session.get(TOKEN_NAME).isDefined){
-				r
-			}
-			else {
-				val session = Cookies(r.header.headers.get("Set-Cookie"))
-					.get(Session.COOKIE_NAME).map(_.value).map(Session.decode)
-					.getOrElse(Map.empty)
-				val newSession = if(session.contains(TOKEN_NAME)) session else (session + (TOKEN_NAME -> token))
-				r.withSession(Session.deserialize(newSession))
-			}
-		}
-		case r => r
-	}
+	def apply(request: Request[A]) = {
+		val token = generate
+		val requestWithToken = addToken(request, token)
 
-	def apply(request: Request[A]): Result = {
-		val r = request.session.get(TOKEN_NAME).map(_ => request).getOrElse(new WrappedRequest(request){
-			override lazy val session = request.session + (TOKEN_NAME -> token)
-		})
-		action(r) match {
-			case ar: AsyncResult => AsyncResult(ar.result.map(addSessionToken(request, _)))
-			case result => addSessionToken(request, result)
-		}
+		checkRequest(requestWithToken)
+			.right.map { r =>
+				action(requestWithToken) match {
+					case ar: AsyncResult => AsyncResult(ar.result.map(addSessionToken(request, _, token)))
+					case result => addSessionToken(request, result, token)
+				}
+			}
+			.fold(identity, identity)
 	}
 
 	lazy val parser = action.parser
