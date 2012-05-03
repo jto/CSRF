dd7f5b6bb2658022ebf3d421b185aefd27394518
jtournay <jtournay@linkedin.com>
2012-05-03 10:47:29 -0700

    org name

--- project/Build.scala
+++ project/Build.scala
@@ -13,6 +13,7 @@ object ApplicationBuild extends Build {
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
+      organization := "jto",
       resolvers += "JTO snapshots" at "https://raw.github.com/jto/mvn-repo/master/snapshots",
       publishTo := Some(Resolver.file("file",  new File(Path.userHome.absolutePath + "/Documents/mvn-repo/snapshots")))
     )
