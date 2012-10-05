417c9bf3376c59e388fd36e1b135b36e13e27d92
jtournay <jtournay@linkedin.com>
2012-10-05 05:13:52 -0700

    upgrade Filters to 2012.09.20.1886ca6.v3

--- project/Build.scala
+++ project/Build.scala
@@ -22,7 +22,7 @@ object ApplicationBuild extends Build {
     }
 
     val pluginDependencies = Seq(
-      "jto" %% "filters" % "2012.08.15.c4c3576.v3", // versions should match
+      "jto" %% "filters" % "2012.09.20.1886ca6.v3", // versions should match
       "commons-codec" % "commons-codec" % "1.6"
     )
 
